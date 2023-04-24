6179d0f74b87e9516872e81265da13abe3d44570
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-04-24 08:39:49 -0400

    WIP

--- polySVG.py
+++ polySVG.py
@@ -1,8 +1,8 @@
-def polySVG(polygon, color):
+def polySVG(polygon, color, strokeWeight='1'):
  # make string for polygon from polygon coordinates
  strA = ''
- for x, y in polygon.exterior.coords:
+ for x, y in polygon.coords:
   strA += str(x) + ',' + str(y) + ' '
   # print(x, y)
- print(strA)
- return ' <polygon points="{0}" fill="{1}" />'.format(strA, color)
+ # print(strA)
+ return ' <polygon points="{0}" fill="{1}" stroke="{1}" stroke-width="{2}" />'.format(strA, color, strokeWeight)
