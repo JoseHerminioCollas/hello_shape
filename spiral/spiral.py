40055fabc926968ce6dda05d446e84008f258340
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-06-21 13:02:44 -0400

    Refactor

--- spiral/spiral.py
+++ spiral/spiral.py
@@ -1,8 +1,6 @@
 import math
 from writeData import writeData
 from writeSVG import writeSVG
-# 'data-1', 350, 0.5, math.pi * 0.15
-# 'data-1', 350, 0.5, 0.1, math.pi * 0.15
 data = writeData('data-1',
                  350,
                  0.5, .01,
