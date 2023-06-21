0062c6addbfbdf7076c821a159d6a355211d4f74
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-06-21 13:30:59 -0400

    Add vector text for logo

--- spiral/spiral.py
+++ spiral/spiral.py
@@ -4,6 +4,6 @@ from writeSVG import writeSVG
 data = writeData('data-1',
                  350,
                  0.5, .01,
-                 2, 0.1,
+                 2, 0.095,
                  math.pi * 0.15)
 writeSVG('spiral-1.svg', data)
