e1eac29734e7f6d64885b64eb97f5b805c175056
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-09-27 17:14:19 -0400

    WIP

--- shapeToSVG.py
+++ shapeToSVG.py
@@ -10,34 +10,40 @@ def pointSVG(cx, cy, color='blue', radius=10):
 def polygonSVG(polygon):
  return ' <polygon points="{0}" fill="red" stroke="black" />'.format(polygon)
 
+def writeDebug(content):
+ f=open('debug.json','w')
+ f.write(content)
+
+def writeSVG(d):
+ content='<path d="{0}" fill="red" stroke="blue" />'.format(d)
+ openTag='<svg viewBox="-100 -100 100 100" width="200" height="200">'
+ bGTag='<rect width="20" height="200" x="-20" y="-20" fill="red" />'
+ openG='<g transform-origin="center" transform="scale(1)">'
+ f=open('countries.svg', 'w')
+ f.write(openTag)
+ f.write(bGTag)
+ f.write(openG)
+ f.write(content)
+ f.write('</g></svg>')
+
+def getSVGPathD(shapelyPolygon):
+ pSVG=shapelyPolygon.svg()
+ regexpA=re.compile(r'd="(.*)"')
+ m=regexpA.search(pSVG)
+ return m.groups()[0]
+
 dataPath="/home/goat/projects/hello_shape/data/countries/ne_10m_admin_0_countries.shp"
 data=ogr.Open(dataPath)
 
-content=pointSVG(10,10)
+# content=pointSVG(10,10)
 debugStr=''
 feature=data[0][3]
-a=feature.ExportToJson()
-b=json.loads(a)
+featureJSON=feature.ExportToJson()
+featureObj=json.loads(featureJSON)
 # has only one entry because it is a polygon
-c=b['geometry']['coordinates'][0]
+featureCoords=featureObj['geometry']['coordinates'][0]
+shapelyPolygon=polygons(featureCoords)
 
-p=polygons(c)
-pSVG=p.svg()
-p=re.compile(r'd="(.*)"')
-m=p.search(pSVG)
-pathValue=m.groups()[0]
-print(pathValue)
-content='<path d="{0}" fill="red" stroke="blue" />'.format(pathValue)
-debugStr=debugStr+a
-openTag='<svg viewBox="-100 -100 100 100" width="200" height="200">'
-bGTag='<rect width="20" height="200" x="-20" y="-20" fill="red" />'
-openG='<g transform-origin="center" transform="scale(1)">'
-f=open('countries.svg', 'w')
-f.write(openTag)
-f.write(bGTag)
-f.write(openG)
-f.write(content)
-f.write('</g></svg>')
-
-f=open('debug.json','w')
-f.write(debugStr)
+pathValue=getSVGPathD(shapelyPolygon)
+writeSVG(pathValue)
+writeDebug(debugStr)
