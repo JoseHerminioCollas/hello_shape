f6df67213dd1b6dbbe22ac6ed9d01afb42f974f9
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-09-27 12:35:44 -0400

    work with the svg data, save output

--- shapeToSVG.py
+++ shapeToSVG.py
@@ -1,9 +1,13 @@
 from osgeo import ogr
 import json
+from shapely import polygons, get_type_id
 
 def pointSVG(cx, cy, color='blue', radius=10):
  z = '<circle r="{3}" cx="{0}" cy="{1}" fill="{2}" stroke="none" />'.format(cx, cy, color, radius)
  return z
+
+def polygonSVG(polygon):
+ return ' <polygon points="{0}" fill="red" stroke="black" />'.format(polygon)
 
 dataPath="/home/goat/projects/hello_shape/data/countries/ne_10m_admin_0_countries.shp"
 data=ogr.Open(dataPath)
@@ -13,16 +17,18 @@ debugStr=''
 feature=data[0][3]
 a=feature.ExportToJson()
 b=json.loads(a)
-c=b['geometry']['coordinates']
+# has only one entry because it is a polygon
+c=b['geometry']['coordinates'][0]
+p=polygons(c)
 
-
-print(b['geometry']['type'])
-debugStr=debugStr+a
+pSVG=p.svg()
+content=pSVG
+debugStr=debugStr+str(c)
 
 f=open('countries.svg', 'w')
-f.write('<svg width="140" height="140" fill="black" stroke="black">')
+f.write('<svg width="100" height="100" fill="black" stroke="none"><g transform="translate(150,75) scale(2)">')
 f.write(content)
-f.write('</svg>')
+f.write('</g></svg>')
 
 f=open('debug.json','w')
 f.write(debugStr)
