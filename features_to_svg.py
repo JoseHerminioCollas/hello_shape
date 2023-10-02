4275e816e30d8034d44d4a35f1d002f8d1cb4443
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-02 15:27:19 -0400

    WIP

--- features_to_svg.py
+++ features_to_svg.py
@@ -2,26 +2,20 @@ from shapely import polygons, from_geojson, buffer, Point
 from get_feature_coords import get_feature_coords
 from get_svg_d_path import get_svg_d_path
 from get_svg_element import get_svg_element
-import json
 import re
-
-def get_path_d(svg_path_tag):
- regexpA=re.compile(r'd="(.*)"')
- m=regexpA.search(svg_path_tag)
- return m.groups()[0]
  
 def features_to_svg(shape_data):
  content=''
 #  range(0,len(shape_data[0]))
- for i in range(0,100):
+ for i in range(0,10):
   feature=shape_data[0][i]
   print(feature.GetField('ISO_N3'))
   jse = feature.ExportToJson()
   shp=from_geojson(jse)  
   shp2=buffer(shp,2,1)
   color='rgba({},{},{},1.0)'.format(i,i,i)
-  svg='<path d="{}" fill="red" />'.format(get_path_d(shp.svg()))
-  svg2='<path d="{}" fill="{}"/>'.format(get_path_d(shp2.svg()), color)
-  content+=svg
-  content+=svg2
+  path_tag=re.sub(r'stroke="#\d+" ', '', shp.svg(0.25, 'green', 1.0))
+  path_tag2=re.sub(r'stroke="#\d+" ', '', shp2.svg(0.25, 'yellow', 1.0))
+  content+=path_tag2
+  content+=path_tag
  return content
--- write_features_to_svg.py
+++ write_features_to_svg.py
@@ -3,7 +3,7 @@ from features_to_svg import features_to_svg
 
 data_path="/home/goat/projects/hello_shape/data/countries/ne_10m_admin_0_countries.shp"
 shape_data=ogr.Open(data_path)
-content='<svg viewBox="-200 150 400 300" width="400" height="300" stroke="gray" stroke-width="0.5" transform="scale(1,-1) translate(0, 0)">'
+content='<svg viewBox="-200 150 400 300" width="400" height="300" stroke="rgba(3,3,3,0.0)" stroke-width="0.0" transform="scale(1,-1) translate(0, 0)">'
 content+='<rect width="100" height="99" x="-100" y="0" fill="blue" stoke-width="0.25"/>'
 content+='<rect width="100" height="99" x="-0" y="0" fill="green"  stoke-width="0.25"/>'
 content+='<rect width="100" height="99" x="100" y="-100" fill="red"  stoke-width="0.25"/>'
