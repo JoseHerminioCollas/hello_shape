ff522b1574e11bd1433a4fc3b1e6dc75a5fbd689
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-02 13:52:39 -0400

    WIP

--- features_to_svg.py
+++ features_to_svg.py
@@ -5,23 +5,23 @@ from get_svg_element import get_svg_element
 import json
 import re
 
+def get_path_d(svg_path_tag):
+ regexpA=re.compile(r'd="(.*)"')
+ m=regexpA.search(svg_path_tag)
+ return m.groups()[0]
+ 
 def features_to_svg(shape_data):
  content=''
 #  range(0,len(shape_data[0]))
  for i in range(0,100):
   feature=shape_data[0][i]
-#   print(feature.GetField('ISO_N3'))
+  print(feature.GetField('ISO_N3'))
   jse = feature.ExportToJson()
   shp=from_geojson(jse)  
-  shp2=buffer(shp, 2,1)
+  shp2=buffer(shp,2,1)
   color='rgba({},{},{},1.0)'.format(i,i,i)
-  # svg=shp2.svg(0.1,color, 1.0)
-  svg=re.sub(
-   'stroke="#\d+"', '', 
-    shp.svg(0.1,color,0.5))
-  svg2=re.sub(
-   'stroke="#\d+"', '', 
-    shp2.svg(0.1,'blue',0.5))
-  content+=svg2
+  svg='<path d="{}" fill="red" />'.format(get_path_d(shp.svg()))
+  svg2='<path d="{}" fill="{}"/>'.format(get_path_d(shp2.svg()), color)
   content+=svg
+  content+=svg2
  return content
