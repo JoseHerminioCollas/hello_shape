986cc23afd65eaaeacb62e7d9dbca8a97c693b09
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-02 08:50:25 -0400

    modify SVG string, refactor

--- features_to_svg.py
+++ features_to_svg.py
@@ -3,17 +3,17 @@ from get_feature_coords import get_feature_coords
 from get_svg_d_path import get_svg_d_path
 from get_svg_element import get_svg_element
 import json
+import re
 
 def features_to_svg(shape_data):
  content=''
- for i in range(0,200):
-  print(i)
+ for i in range(0,len(shape_data[0])):
   feature=shape_data[0][i]
-  jse = feature.ExportToJson()
-#   make a shapely object
-  shp=from_geojson(jse)  
-  svg=shp.svg()
-  content+=svg
-  print('svg',svg)
   print(feature.GetField('ISO_N3'))
+  jse = feature.ExportToJson()
+  shp=from_geojson(jse)  
+  color='rgba({},{},{},1.0)'.format(i,i,i)
+  svg=shp.svg(0.1,color, 1.0)
+  svg2=re.sub('stroke="#\d+"', '', svg)
+  content+=svg2
  return content
