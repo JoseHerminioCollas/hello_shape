a0cf0622b362713f7656098b5292b57c2828f398
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-01 20:50:01 -0400

    WIP

--- features_to_svg.py
+++ features_to_svg.py
@@ -6,21 +6,39 @@ import json
 
 def features_to_svg(shape_data):
  content=''
- for f in shape_data[0]:
-  j=f.ExportToJson()
-  k=json.loads(j)
-  geo_type=k['geometry']['type']
-  print('geo type', geo_type, len(k['geometry']['coordinates']))
-  feature_coords=k['geometry']['coordinates']
-  fc=[]
-  if geo_type=='MultiPolygon':
-   fcs=feature_coords[0]
-  if geo_type=='Polygon':
-   fcs=feature_coords
-  for fc in fcs:
-   shapely_polygon=polygons(fc)
-   d_path_value=get_svg_d_path(shapely_polygon)
-   content+='<g transform-origin="center" transform="scale(1)">'
-   content+='<path d="{0}" fill="red" stroke="green" />'.format(d_path_value)
-   content+='</g>'
+ for i in range(0,200):
+  f=shape_data[0][i]
+  jse = f.ExportToJson(True)
+  props=jse['properties']
+  idd=jse['properties']['ISO_N3']
+  if idd=='840':
+#    print('usa', props)
+#    print(jse['geometry'])
+#   print(idd)
+#   for i in range(f.GetFieldCount()):
+#    print('xxxx', f.GetFieldDefnRef(i).GetName())
+#    print('xxxx', f.GetFieldDefnRef(i).GetType())
+#    print(f.GetGeomFieldCount())
+#   for i in range(f.GetGeomFieldCount()):
+#    print('a', f.GetGeomFieldDefnRef(i).GetName())
+#    print(f.GetGeomFieldDefn(i).GetType())
+# GetGeomFieldDefn(i).GetType()
+#   j=f.ExportToJson()
+#   k=json.loads(j)
+   geo_type=jse['geometry']['type']
+#   print('geo type', geo_type, len(k['geometry']['coordinates']))
+   feature_coords=jse['geometry']['coordinates']
+#   fc=[]
+   if geo_type=='MultiPolygon':
+    fcs=feature_coords
+    print('xxx', len(feature_coords))
+   if geo_type=='Polygon':
+    fcs=feature_coords
+   for fc in fcs:
+     print('333',len(fc))
+     shapely_polygon=polygons(fc[0])
+     d_path_value=get_svg_d_path(shapely_polygon)
+     content+='<g transform-origin="center" transform="scale(1)">'
+     content+='<path d="{0}" fill="red" stroke="green" />'.format(d_path_value)
+     content+='</g>'
  return content
