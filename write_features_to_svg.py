b79e5f619d463f0900c0094537c815d3b8a4eabc
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-04 10:55:55 -0400

    update script to generate all features

--- write_features_to_svg.py
+++ write_features_to_svg.py
@@ -1,8 +1,20 @@
 from osgeo import ogr
-from feature_to_svg import feature_to_svg
+from shapely import from_geojson, buffer
+from shape_to_svg import shape_to_svg
+from get_svg_document import get_svg_document
 
 data_path="/home/goat/projects/hello_shape/data/countries/ne_10m_admin_0_countries.shp"
-shape_data=ogr.Open(data_path)
+countries_data=ogr.Open(data_path)
+
+countries_layer=countries_data[0]
+for feature in countries_layer:
+ geojson = feature.ExportToJson()
+ shape_one=from_geojson(geojson)
+ shape_two=buffer(shape_one,2,1)
+ shape_feature_svg=shape_to_svg(shape_one, 'red')
+ shape_feature_svg_two=shape_to_svg(shape_two, 'blue')
+
+svg_document=get_svg_document(shape_feature_svg_two+shape_feature_svg)
 
 f=open('generated/features.svg', 'w')
-f.write('content')
+f.write(svg_document)
