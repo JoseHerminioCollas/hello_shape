3837a71c68048c20a22bfaaad7d60fec6aca4cd7
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-10 14:55:51 -0400

    WIP

--- get_svg_document.py
+++ get_svg_document.py
@@ -1,6 +1,6 @@
 def get_svg_document(inner_contents):
 # -50 -50 100 200" width="200" height="200"
-    content='<svg viewBox="-300 -300 600 600" width="600" height="600" stroke="green" stroke-width="1">'
+    content='<svg viewBox="-300 -300 600 600" width="600" height="600" stroke="green" stroke-width="1" transform="scale(1,1)">'
     content+='<rect x="0" y="0" width="290" height="290" fill="green" />'
     content+=inner_contents
     content+='</svg>'
--- write_features_to_svg.py
+++ write_features_to_svg.py
@@ -7,25 +7,29 @@ data_path='/home/goat/projects/hello_shape/data/Madrid-shp/shape/buildings.shp'
 data_source=ogr.Open(data_path)
 layers=data_source[0]
 polygons=[]
-for i in range(0,20):
+items_num=200
+for i in range(0,items_num):
  feature=layers[i]
  name=feature.GetField('NAME')
- print('name: ',name)
+#  print('name: ',name)
  geojson=feature.ExportToJson()
  shape_one=from_geojson(geojson)
- shape_two=affinity.scale(shape_one, 20,20)
+ shape_two=affinity.scale(shape_one,7,7)
  polygons.append(shape_two)
 
 doc=''
 multi_polygon = MultiPolygon(polygons)
-m_p_scaled=affinity.scale(multi_polygon,1500,1500)
+m_p_scaled=affinity.scale(multi_polygon,2000,2000)
 m_p_list=list(m_p_scaled.geoms)
-for m_p in m_p_list:
- b=Polygon(m_p)
- str='<text size="44" x="{}" y="{}" fill="black" stroke="red">XXX</text>'
- doc+=str.format(b.centroid.x,b.centroid.y)
+doc+=m_p_scaled.svg(1,'blue', 1)
 
-doc+=m_p_scaled.svg(1,'green', 1)
+for j in range(0,items_num):
+ poly=Polygon(m_p_list[j])
+ poly_info=layers[j].ExportToJson(True)
+ print('xx',poly_info['properties']['name'])
+ str='<text font-size="2" x="{}" y="{}" fill="black" stroke="none">{}</text>'
+ doc+=str.format(poly.centroid.x,poly.centroid.y,poly_info['properties']['name'])
+
 svg_document=get_svg_document(doc)
 
 f=open('generated/featuresx.svg', 'w')
