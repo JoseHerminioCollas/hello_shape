63947e6ee8713384bdc994881de03b8229fff80b
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-10 16:01:48 -0400

    WIP

--- write_features_to_svg.py
+++ write_features_to_svg.py
@@ -7,14 +7,14 @@ data_path='/home/goat/projects/hello_shape/data/Madrid-shp/shape/buildings.shp'
 data_source=ogr.Open(data_path)
 layers=data_source[0]
 polygons=[]
-items_num=200
+items_num=1100
 for i in range(0,items_num):
  feature=layers[i]
  name=feature.GetField('NAME')
 #  print('name: ',name)
  geojson=feature.ExportToJson()
  shape_one=from_geojson(geojson)
- shape_two=affinity.scale(shape_one,7,7)
+ shape_two=affinity.scale(shape_one,3,3)
  polygons.append(shape_two)
 
 doc=''
@@ -27,8 +27,8 @@ for j in range(0,items_num):
  poly=Polygon(m_p_list[j])
  poly_info=layers[j].ExportToJson(True)
  print('xx',poly_info['properties']['name'])
- str='<text font-size="2" x="{}" y="{}" fill="black" stroke="none">{}</text>'
- doc+=str.format(poly.centroid.x,poly.centroid.y,poly_info['properties']['name'])
+ str='<text font-size="9" x="{}" y="{}" fill="black" stroke="none">{}</text>'
+#  doc+=str.format(poly.centroid.x,poly.centroid.y,poly_info['properties']['name'])
 
 svg_document=get_svg_document(doc)
 
