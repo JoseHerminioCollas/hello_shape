1c45e9fb30161b3ec782b9146f046b607e41e92d
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-13 12:36:57 -0400

    WIP

---
+++ get_text_svg_element.py
@@ -0,0 +1,3 @@
+def get_text_svg_element(a,b,c):
+ s='<text font-size="3" x="{}" y="{}" fill="black" stroke="none">{}</text>'
+ return s.format(a,b,c)
--- write_features_to_svg.py
+++ write_features_to_svg.py
@@ -3,22 +3,27 @@ from shapely import  affinity,MultiLineString
 from get_svg_document import get_svg_document
 from get_geo_data import get_geo_data
 from get_text_svg import get_text_svg
+from get_text_svg_element import get_text_svg_element
 
 data_path='/home/goat/projects/hello_shape/data/Madrid-shp/shape/roads.shp'
 data_source=ogr.Open(data_path)
 layers=data_source[0]
 # OGR features to geo json Python data
 geo_data=get_geo_data(layers,3000)
+
 # scale with a MultiLineString
 coords=[]
 for s in geo_data:
  coords.append(s['geometry']['coordinates'])
 multi_line_string = MultiLineString(coords)
-scale_factor=700
-mls_scaled=affinity.scale(multi_line_string,scale_factor,scale_factor)
+mls_scaled=affinity.scale(multi_line_string,700,700)
 mls_scaled_list=list(mls_scaled.geoms)
-# write to the doc object and write the file
-doc=get_text_svg(geo_data,mls_scaled_list)
+# geo_data_orig_size geo_data_scaled
+
+doc=''
+for gd in geo_data:
+ doc+=get_text_svg_element(1,1,'xx')
 doc+=mls_scaled.svg(0.25,'black', 1.0)
+
 f=open('generated/features_10_13_2023.svg', 'w')
 f.write(get_svg_document(doc))
