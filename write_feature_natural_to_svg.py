3d26857b520565e26fbfc09f5a59049760a15929
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-13 15:43:36 -0400

    WIP

--- write_feature_natural_to_svg.py
+++ write_feature_natural_to_svg.py
@@ -5,7 +5,7 @@ from get_svg_document import get_svg_document
 from get_geo_data import get_geo_data
 from get_text_svg_element import get_text_svg_element
 
-data_path='/home/goat/projects/hello_shape/data/Madrid-shp/shape/natural.shp'
+data_path='/home/goat/projects/hello_shape/data/Madrid-shp/shape/landuse.shp'
 data_source=ogr.Open(data_path)
 layers=data_source[0]
 # OGR features to geo json Python data
@@ -14,10 +14,11 @@ geo_data=get_geo_data(layers,1000)
 shapely_objects=[]
 for i in range(0,len(geo_data)):
  el=from_geojson(json.dumps(geo_data[i]))
- shapely_objects.append(el)
+ el2=affinity.scale(el,1,1)
+ shapely_objects.append(el2)
 
 mp=MultiPolygon(shapely_objects)
-mp2=affinity.scale(mp,1000,1000)
+mp2=affinity.scale(mp,2000,2000)
 
 doc=''
 doc+=mp2.svg(1,'red',1.0)
