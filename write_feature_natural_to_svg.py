2b0503bc7804f222d1100ce2e52eb4d9c98a318d
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-18 12:45:13 -0400

    WIP

--- write_feature_natural_to_svg.py
+++ write_feature_natural_to_svg.py
@@ -5,31 +5,38 @@ from get_svg_document import get_svg_document
 
 data_path='/home/goat/projects/hello_shape/data/Madrid-shp/shape/natural.shp'
 data_source=ogr.Open(data_path)
-sql="SELECT * FROM natural where type='park' limit 300"
-layer=data_source.ExecuteSQL(sql)
-print(layer.GetFeatureCount())
+gr=ogr.CreateGeometryFromWkt('POLYGON ((-3.8 40,-3.4 40.5,-3 40.5,-3 40.25,-3.8 40))')
+print(gr)
+# ,[(0,0),(0,1),(1,1),(1,0)] (-4.0699641, -3.2300143, 40.170042, 40.6499669)
+sql="SELECT * FROM natural where type='park' limit 1300"
+layer=data_source.ExecuteSQL(sql,gr )
+e=layer.GetExtent()
+print(e)
+# one_data_source_for_all ????
+geo_data_features=[]
 for feature in layer:
- print(feature.ExportToJson())
-geo_data=[]
-for feature in layer:
- geo_data.append(feature.ExportToJson(True))
+#  print(feature.geometry())
+ geo_data_features.append(feature.ExportToJson(True))    
+
 polygons=[
  affinity.scale(from_geojson(json.dumps(gd)),3,3)
-  for gd in geo_data]
+  for gd in geo_data_features]
 multi_polygon=MultiPolygon(polygons)
-multi_polygon_scaled=affinity.scale(multi_polygon,2000,-2000)
+multi_polygon_scaled=affinity.scale(multi_polygon,200,-200)
+
 # create the document, write the file
 doc=''
+doc+=affinity.scale(from_geojson(gr.ExportToJson()),200,200).svg(1,'blue',0.5)
 # doc+=multi_polygon_scaled.svg(1,'gray',0.5)
-for i in range(0,len(geo_data)):
+svg_text='<text font-size="{}" x="{}" y="{}" fill="{}" stroke="none">{}</text>'
+for i in range(0,len(geo_data_features)):
  shapely_polygon=multi_polygon_scaled.geoms[i]
- doc+=shapely_polygon.svg(1,'blue',0.5)
- svg_text='<text font-size="{}" x="{}" y="{}" fill="{}" stroke="none">{}</text>'
- doc+=svg_text.format(
-  7,
-  shapely_polygon.centroid.x, shapely_polygon.centroid.y,
-  'purple',
-  geo_data[i]['properties']['name'])
+ doc+=shapely_polygon.svg(1,'red',0.5)
+#  doc+=svg_text.format(
+#   11,
+#   shapely_polygon.centroid.x, shapely_polygon.centroid.y,
+#   'purple',
+#   geo_data_features[i]['properties']['name'])
 
-j=open('generated/features_10_17_2023.svg', 'w')
+j=open('generated/features_10_18_2023.svg', 'w')
 j.write(get_svg_document(doc))
