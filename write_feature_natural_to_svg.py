50eafc108931a4545109aed1fbcc00c4ba02a0a8
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-10-30 15:51:42 -0400

    WIP

--- write_feature_natural_to_svg.py
+++ write_feature_natural_to_svg.py
@@ -8,22 +8,27 @@ from get_svg import get_svg
 data_path='/home/goat/projects/hello_shape/data/Madrid-shp/shape/natural.shp'
 data_source=ogr.Open(data_path)
 
-item_scale=1
-group_scale=2000
-limit=1000
+limit=100
 sql="SELECT * FROM natural where type='park' and name is not null limit {}".format(limit)
 layer=data_source.ExecuteSQL(sql)
 
 features=Features(layer)
-features.polygons=[
- affinity.scale(from_geojson(json.dumps(gd)),item_scale,item_scale)
-  for gd in features.data]
-features.multi_polygon=MultiPolygon(features.polygons)
-features.multi_polygon_scaled=affinity.scale(features.multi_polygon,group_scale,-group_scale)
+for f in features.scaled:
+ print('f')
 
-doc=[
- get_svg(features.multi_polygon_scaled.geoms[i],features.data[i])
-  for i in range(0,len(features.data))]
+for p in features.poygons:
+ print(p)
+item_scale=1
+group_scale=2000
+# features.polygons=[
+#  affinity.scale(from_geojson(json.dumps(gd)),item_scale,item_scale)
+#   for gd in features.data]
+# features.multi_polygon=MultiPolygon(features.polygons)
+# features.multi_polygon_scaled=affinity.scale(features.multi_polygon,group_scale,-group_scale)
 
-j=open('generated/features_10_19_2023.svg', 'w')
-j.write(get_svg_document(''.join(doc)))
+# doc=[
+#  get_svg(features.multi_polygon_scaled.geoms[i],features.data[i])
+#   for i in range(0,len(features.data))]
+
+# j=open('generated/features_10_19_2023.svg', 'w')
+# j.write(get_svg_document(''.join(doc)))
