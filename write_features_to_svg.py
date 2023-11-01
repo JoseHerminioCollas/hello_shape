d4fca686fc721b9184c66fc54ce379cf341eac0c
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-01 05:42:52 -0400

    WIP

--- write_features_to_svg.py
+++ write_features_to_svg.py
@@ -1,28 +1,33 @@
 from osgeo import ogr
 from Features import Features
 from SVGTag import SVGTag
+def madrid_parks(data_path,sql):
+ data_source=ogr.Open(data_path)
+ layer=data_source.ExecuteSQL(sql)
+ features=Features(layer)
+ features.print_info()
+ svg_tag=SVGTag()
+ for i in range(0,len(features.data)):
+  svg_tag.set_polygon(features.scaled_group.geoms[i].svg())
+  svg_tag.set_text_element(
+   features.data[i]['properties']['name'],
+   features.scaled_group.geoms[i].centroid.x, features.scaled_group.geoms[i].centroid.y,
+   3
+  )
+ j=open('generated/madrid_parks_2023_11_1.svg', 'w')
+ j.write(svg_tag.render())
 
-# set the layer that will be used as an argument to the Features class
-data_path='/home/goat/projects/hello_shape/data/Madrid-shp/shape/natural.shp'
-data_source=ogr.Open(data_path)
-sql=("SELECT * FROM natural where type='park' and name is not null limit {}"
- .format(600))
-layer=data_source.ExecuteSQL(sql)
-# set up the two main classes Features and SVGTag
-features=Features(layer)
-features.print_info()
-svg_tag=SVGTag()
-# build the SVGTag
-for i in range(0,len(features.data)):
- svg_tag.set_polygon(features.scaled_group.geoms[i].svg())
- svg_tag.set_text_element(
-  features.data[i]['properties']['name'],
-  # -300, (300 - (i * 4)),
-  features.scaled_group.geoms[i].centroid.x, features.scaled_group.geoms[i].centroid.y,
-  1
- )
-# render the SVGTag
-j=open('generated/features_10_31_2023.svg', 'w')
-j.write(svg_tag.render())
-# -300, (300 - (i * 6)),
-# features.scaled_group.geoms[i].centroid.x, features.scaled_group.geoms[i].centroid.y,
+calls=[
+{
+ 'path':
+  'data/Madrid-shp/shape/natural.shp',
+ 'sql':
+  "SELECT * FROM natural where type='park' and name is not null limit {}"
+   .format(60),
+ 'script':madrid_parks
+}]
+for i in range(0,len(calls)):
+ print(calls[i])
+ path=calls[i]['path']
+ sql=calls[i]['sql']
+ calls[i]['script'](path,sql)
