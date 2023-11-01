dfcb09e66e523aea0ac7d83108725bcee0cf4a42
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-01 05:52:33 -0400

    WIP

--- write_features_to_svg.py
+++ write_features_to_svg.py
@@ -1,21 +1,4 @@
-from osgeo import ogr
-from Features import Features
-from SVGTag import SVGTag
-def madrid_parks(data_path,sql):
- data_source=ogr.Open(data_path)
- layer=data_source.ExecuteSQL(sql)
- features=Features(layer)
- features.print_info()
- svg_tag=SVGTag()
- for i in range(0,len(features.data)):
-  svg_tag.set_polygon(features.scaled_group.geoms[i].svg())
-  svg_tag.set_text_element(
-   features.data[i]['properties']['name'],
-   features.scaled_group.geoms[i].centroid.x, features.scaled_group.geoms[i].centroid.y,
-   3
-  )
- j=open('generated/madrid_parks_2023_11_1.svg', 'w')
- j.write(svg_tag.render())
+from madrid_parks import madrid_parks
 
 calls=[
 {
@@ -23,11 +6,14 @@ calls=[
   'data/Madrid-shp/shape/natural.shp',
  'sql':
   "SELECT * FROM natural where type='park' and name is not null limit {}"
-   .format(60),
- 'script':madrid_parks
+   .format(200),
+ 'script':madrid_parks,
+ 'destination':'generated/madrid_parks_2023_11_1.svg'
 }]
 for i in range(0,len(calls)):
  print(calls[i])
  path=calls[i]['path']
  sql=calls[i]['sql']
- calls[i]['script'](path,sql)
+ destination=calls[i]['destination']
+ # call the provided function to run the script
+ calls[i]['script'](path,sql,destination)
