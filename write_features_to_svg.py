548c8847cfcf6026ea60f36f23a92eaeabac5006
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-01 06:11:05 -0400

    delete unused generated files, add function for Madrid buildings

--- write_features_to_svg.py
+++ write_features_to_svg.py
@@ -1,5 +1,5 @@
 from madrid_parks import madrid_parks
-
+from madrid_buildings import madrid_buildings
 calls=[
 {
  'path':
@@ -8,8 +8,17 @@ calls=[
   "SELECT * FROM natural where type='park' and name is not null limit {}"
    .format(200),
  'script':madrid_parks,
- 'destination':'generated/madrid_parks_2023_11_1.svg'
-}]
+ 'destination':'generated/madrid_parks_2023_11_1.svg'},
+ {
+  'path':
+   'data/Madrid-shp/shape/buildings.shp',
+  'sql':
+   "SELECT * FROM buildings where name is not null limit {}"
+   .format(200),
+  'script': madrid_buildings,
+  'destination': 'generated/madrid_buildings_2023_11_1.svg'
+ }
+]
 for i in range(0,len(calls)):
  print(calls[i])
  path=calls[i]['path']
