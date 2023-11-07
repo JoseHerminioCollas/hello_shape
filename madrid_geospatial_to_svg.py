20b4bc80cf58978960dd54e2407ad284a273a9c7
Jose Collas <joseherminiocollas@goatstone.com>
2023-11-07 11:24:12 -0500

    Add ENV VARS to script

--- madrid_geospatial_to_svg.py
+++ madrid_geospatial_to_svg.py
@@ -1,14 +1,22 @@
+import os
 from madrid_parks import madrid_parks
 from madrid_buildings import madrid_buildings
+
+if('DATA_PATH' not in os.environ.keys()
+   or  'FILE_DESTINATION' not in os.environ.keys()):
+ print('Error: Environmental Variables not defined' )
+ exit()
+ 
 calls=[
 {
  'path':
-  'data/Madrid-shp/shape/natural.shp',
+  os.environ['DATA_PATH'],
  'sql':
   "SELECT * FROM natural where type='park' and name is not null limit {}"
    .format(200),
  'script':madrid_parks,
- 'destination':'generated/madrid_parks_2023_11_2.svg'},
+ 'destination':os.environ.get('FILE_DESTINATION'),
+}
 ]
 for i in range(0,1):
  print(calls[i])
