b25e18fe351a60e3be20f9d1c2e511637f68846e
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-09 20:27:36 -0500

    add script to run function

--- run_script.py
+++ run_script.py
@@ -1,4 +1,18 @@
 import madrid_parks from madrid_parks
 import sys
+from osgeo import ogr
+limit=200
+config =     {
+        'path':
+            'data/Madrid-shp/shape/natural.shp',
+        'sql':
+            "SELECT * FROM natural where type='park' and name is not null limit {}"
+            .format(3),
+        'destination': os.environ.get('FILE_DESTINATION'),
+    }
+
 # layer destination_path
+   data_source = ogr.Open(data_path)
+    layer = data_source.ExecuteSQL(sql)
+ 
 madrid_parks(sys.argv[0], sys.argv[1], sys.argv2)
