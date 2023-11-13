22ac43af7e8ff61248d7828a3fe3925a60115ce6
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-13 11:30:56 -0500

    refactor, do not provide SVGTag as argument

--- madrid_parks.py
+++ madrid_parks.py
@@ -1,19 +1,22 @@
 from Features import Features
-from SVGTag import SVGTag
+
+svg_text = '<text   x="{}" y="{}" fill="{}" stroke="none" text-anchor="middle">{}</text>'
+
 
 def madrid_parks(
-        svg_tag,
         layer,
         item_scale=3,
         group_scale=1000,
         font_size=5
 ):
     features = Features(layer, item_scale, group_scale)
+    svg = ''
     for i in range(0, len(features.data)):
-        svg_tag.set_polygon(features.scaled_group.geoms[i].svg())
-        svg_tag.set_text_element(
-            features.data[i]['properties']['name'],
-            features.scaled_group.geoms[i].centroid.x, features.scaled_group.geoms[i].centroid.y,
-            font_size
-        )
-    return svg_tag
+        svg += features.scaled_group.geoms[i].svg()
+        svg += ('<text   x="{}" y="{}" text-anchor="middle">{}</text>'
+        .format(
+            features.scaled_group.geoms[i].centroid.x,
+            features.scaled_group.geoms[i].centroid.y,
+            features.data[i]['properties']['name']
+        ))
+    return svg
--- run_script.py
+++ run_script.py
@@ -7,22 +7,29 @@ item_scale = 3
 group_scale = 1500
 limit = 1000
 data_path = 'data/Madrid-shp/shape/natural.shp'
-destination_path = '__generated/madrid_parks_11_10.svg'
+destination_path = '__generated/madrid_parks_11_13.svg'
 spat_box = box(-3.5, 40.17, -3.2, 40.6)
 spat_box_scaled = affinity.scale(spat_box, group_scale, group_scale)
-print(spat_box)
-wkt_spatial_filter = "POLYGON ((-3.5 40.17, -3.2 40.17, -3.2 40.6, -3.5 40.6, -3.5 40.17))"
 sql = ("SELECT * FROM natural where type='park' and name is not null limit {}"
        .format(limit))
 data_source = ogr.Open(data_path)
+print('data_source created: ', data_source)
+
 layer = data_source.ExecuteSQL(
     sql,
     ogr.CreateGeometryFromWkt(to_wkt(spat_box))
 )
-print(len(layer))
+print(len('layer created: '), layer)
+
 svg_tag = SVGTag()
-svg_tag.set_polygon(spat_box_scaled.svg())
-madrid_parks(svg_tag, layer, item_scale, group_scale)
+svg_tag.prepend(
+    spat_box_scaled.svg()
+)
+svg_tag.append(
+    madrid_parks(layer, item_scale, group_scale)
+)
+print('SVG tag created: ', svg_tag)
 
 file = open(destination_path, 'w')
 file.write(svg_tag.render())
+print('File created: ', file)
