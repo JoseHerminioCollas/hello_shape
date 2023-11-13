11cff94a438621c0836572c275a3454bb96bd5c7
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-13 15:07:11 -0500

    use style sheet to define style

--- SVGTag.py
+++ SVGTag.py
@@ -1,27 +1,25 @@
 class SVGTag:
     name = 'SVGTag'
+    side=600
 
     def __init__(self):
+        f = open('style.css', 'r', encoding='utf-8')
+        self.style=f.read()
         self.doc = ''
 
     def render(self):
-        svg_doc = '<svg viewBox="-300 -300 600 600" width="600" height="600" stroke="green" stroke-width="1">'
-        svg_doc += ('<style>'
-                    'text {font-family:sans-serif;font-size:3px;fill:rgba(3,3,3,0.5);stroke:blue;stroke-width:0}'
-                    'path {fill:gold; stroke:rgba(4,4,4,0.5); stroke-width:1px;}'
-                    '</style>')
+        svg_doc = '<svg viewBox="-300 -300 {s} {s}" width="{s}" height="{s}">'.format(s=self.side)
+        svg_doc += '<style>'+self.style+'</style>'
         svg_doc += self.doc
         svg_doc += '</svg>'
         return svg_doc
 
     def set_text_element(self, name, x, y, font_size=9, color='rgba(3,3,113,0.5)'):
-        svg_text = '<text   x="{}" y="{}" fill="{}" stroke="none" text-anchor="middle">{}</text>'
+        svg_text = '<text x="{}" y="{}">{}</text>'
         self.doc += svg_text.format(x, y, color, name)
-# prepend_svg append_svg TODO
-    def set_polygon(self, doc_str):
-        self.doc += doc_str
-        return True
-    def append(self,svg):
+
+    def append(self, svg):
         self.doc += svg
-    def prepend(self,svg):
+
+    def prepend(self, svg):
         self.doc = svg + self.doc
--- madrid_parks.py
+++ madrid_parks.py
@@ -1,22 +1,20 @@
 from Features import Features
 
-svg_text = '<text   x="{}" y="{}" fill="{}" stroke="none" text-anchor="middle">{}</text>'
-
 
 def madrid_parks(
         layer,
         item_scale=3,
         group_scale=1000,
-        font_size=5
 ):
     features = Features(layer, item_scale, group_scale)
-    svg = ''
+    svg = '<g class="parks">'
     for i in range(0, len(features.data)):
         svg += features.scaled_group.geoms[i].svg()
-        svg += ('<text   x="{}" y="{}" text-anchor="middle">{}</text>'
+        svg += ('<text x="{}" y="{}">{}</text>'
         .format(
             features.scaled_group.geoms[i].centroid.x,
             features.scaled_group.geoms[i].centroid.y,
             features.data[i]['properties']['name']
         ))
+    svg += '</g>'
     return svg
