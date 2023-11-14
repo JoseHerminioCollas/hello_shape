abe330006707705d15949e31658d5260ce5829b3
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-14 13:07:25 -0500

    WIP

--- run_script.py
+++ run_script.py
@@ -4,19 +4,25 @@ from shapely import to_wkt, affinity, box
 from osgeo import ogr
 
 item_scale = 3
-group_scale = 1500
-limit = 1000
+group_scale = 2000
 data_path = 'data/Madrid-shp/shape/natural.shp'
 destination_path = '__generated/madrid_parks_11_14.svg'
-spat_box = box(-3.5, 40.17, -3.2, 40.6)
+spat_box = box(-3.8, 40.2, -3.3, 40.5)
 spat_box_scaled = affinity.scale(spat_box, group_scale, group_scale)
-sql = ("SELECT * FROM natural where type='park' and name is not null limit {}"
-       .format(limit))
+sql_park = ("SELECT * FROM natural where type='park' and name is not null limit {}"
+       .format(200))
+sql_water = ("SELECT * FROM natural where type='water' and name is not null limit {}"
+       .format(3000))
+
 data_source = ogr.Open(data_path)
 print('data_source created: ', data_source)
 
 layer = data_source.ExecuteSQL(
-    sql,
+    sql_park,
+    ogr.CreateGeometryFromWkt(to_wkt(spat_box))
+)
+layer2 = data_source.ExecuteSQL(
+    sql_water,
     ogr.CreateGeometryFromWkt(to_wkt(spat_box))
 )
 print(len('layer created: '), layer)
@@ -28,6 +34,9 @@ svg_tag.prepend(
 )
 svg_tag.append(
     layer_to_svg(layer, 'parks', item_scale, group_scale)
+)
+svg_tag.append(
+    layer_to_svg(layer2, 'water', item_scale, group_scale)
 )
 print('SVG tag created: ', svg_tag)
 
