161e71add627668c3e42a15144236870d4f696b3
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-17 15:17:58 -0500

    add function name and style to command line method

--- command_line_example.sh
+++ command_line_example.sh
@@ -1,7 +1,7 @@
-s="from madrid_natural import madrid_natural;"
-s+="f=open('style.css','r',encoding='utf-8');"
+s="from $FUNC_NAME import $FUNC_NAME;"
+s+="f=open('${STYLE_PATH}','r',encoding='utf-8');"
 s+="style=f.read();"
-s+="svg=madrid_natural(${ITEM_SCALE},${GROUP_SCALE},'${DATA_PATH}',style);"
+s+="svg=$FUNC_NAME(${ITEM_SCALE},${GROUP_SCALE},'${DATA_PATH}',style);"
 s+="f=open('${DEST_PATH}','w');f.write(svg)"
 echo $s
 
