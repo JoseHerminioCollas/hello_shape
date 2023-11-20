f67892b200f18a075455bbd072f89f5f95375bf1
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-20 15:23:48 -0500

    refactor command line script

--- py.sh
+++ py.sh
@@ -1,8 +1,7 @@
 # example of use of py.sh
-# ./py.sh madrid.natural natural 2 3 data/Madrid-shp/shape/natural.shp > file.svg
+# ./py.sh madrid.natural natural 2 3 data/Madrid-shp/shape/natural.shp __generated/file.svg
+# package function item_scale group_scale data_path style_sheet_path destination
 
 s="from $1 import $2;"
-s+="svg=$2(${3},${4},'${5}','');"
-s+="print(svg)"
-
-python3 -c "${s}"
+s+="$2($3,$4,'$5','$6');";
+python3 -c "${s}" > $7
