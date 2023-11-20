58ff3213192ea11b3a103ed3c60a805d597e2dc6
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-20 16:49:12 -0500

    provide style path, get contents of file for style defs

--- py.sh
+++ py.sh
@@ -1,8 +1,16 @@
-# example of use of py.sh
-# ./py.sh madrid.natural natural 2 3 data/Madrid-shp/shape/natural.shp __generated/file.svg
+# example of ucatof py.sh
+# ./py.sh adrid.natural natural 2 3 data/Madrid-shp/shape/natural.shp __generated/file.svg
 # package function item_scale group_scale data_path style_sheet_path destination
 
+#  make this into a single line, strip out returns
+f=`cat ./madrid/style.css`
+g="${f//[$'\t\r\n']}"
+echo $g
+
+
 s="from $1 import $2;"
-s+="svg = $2($3,$4,'$5','$6');";
+s+="svg = $2($3,$4,'$5','$g');";
 s+="print(svg)"
+echo $s
 python3 -c "${s}" > $7
+
