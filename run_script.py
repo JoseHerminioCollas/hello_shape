10dfca00c01a0c0220fd77bdcb740af16360fa9d
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-21 12:46:49 -0500

    update runscript to replace image

--- run_script.py
+++ run_script.py
@@ -6,7 +6,7 @@ group_scale = 10000
 data_path = 'data/Madrid-shp/shape/natural.shp'
 file = open('madrid/style.css', 'r', encoding='utf-8') 
 t=time.strftime('%X', time.localtime())
-destination_path = '__generated/madrid_parks_11_20{}.svg'.format(t)
+destination_path = '__generated/madrid_2023_11_21{}.svg'.format('')
 
 svg_tag=natural(item_scale,group_scale,data_path,file.read())
 
