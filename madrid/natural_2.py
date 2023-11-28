fbe94a82969b2c66f5424b090686fe814a30d22d
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-28 08:37:11 -0500

    WIP

--- madrid/natural_2.py
+++ madrid/natural_2.py
@@ -5,7 +5,7 @@ from osgeo import ogr
 
 def natural_2(item_scale,group_scale,data_path,styles,spat_zoom=0,center=None):
     sql_park = ("SELECT * FROM natural where type='park' and name is not null limit {}"
-        .format(500))
+        .format(50000))
     data_source = ogr.Open(data_path)
     layer=data_source.GetLayer()
     extent=layer.GetExtent() # extent (-4.0699641, -3.2300143, 40.170042, 40.6499669)
@@ -13,14 +13,14 @@ def natural_2(item_scale,group_scale,data_path,styles,spat_zoom=0,center=None):
     exmidy=extent[2]-((extent[2]-extent[3])/2)
     # print('extent', extent, exmidx)
     p=Point(exmidx+0.05,exmidy+0.04)
-    point_buff=p.buffer(0.1)
+    point_buff=p.buffer(0.2)
     point_bounds=point_buff.bounds
     sbox=box(point_bounds[0],point_bounds[1],point_bounds[2],point_bounds[3])
     park_layer = data_source.ExecuteSQL(
         sql_park,    
          ogr.CreateGeometryFromWkt(to_wkt(sbox))
     )
-    scale=2500
+    scale=1000
     polys=[]
     for f in park_layer:
         geo_json = f.ExportToJson()
@@ -38,5 +38,5 @@ def natural_2(item_scale,group_scale,data_path,styles,spat_zoom=0,center=None):
     svg_tag = SVGTag(styles)
     # svg_tag.prepend(spat_box2_scaled.svg(),'extent-level-1')
     svg_tag.append(sbox.svg(),'target_area')
-    svg_tag.append(scaled_poly.svg(), 'extent-level-2')
+    svg_tag.append(scaled_poly.svg(), '')
     return svg_tag.render()
