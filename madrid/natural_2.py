8253c8944ebc4d2c06c45bc7e851deec49eebd83
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-29 08:52:25 -0500

    WIP

--- madrid/natural_2.py
+++ madrid/natural_2.py
@@ -1,42 +1,51 @@
 from Features import Features
 from SVGTag import SVGTag
-from shapely import MultiPolygon, affinity, box, Point, from_geojson,is_valid, to_wkt
+from shapely import MultiPolygon, affinity, box, Point, from_geojson, is_valid, to_wkt
 from osgeo import ogr
 
-def natural_2(item_scale,group_scale,data_path,styles,spat_zoom=0,center=None):
+
+def natural_2(
+        item_scale,
+        group_scale,
+        data_path,
+        styles,
+        spatial_filter=None,
+        center=None
+):
     sql_park = ("SELECT * FROM natural where type='park' and name is not null limit {}"
-        .format(50000))
+                .format(50000))
     data_source = ogr.Open(data_path)
-    layer=data_source.GetLayer()
-    extent=layer.GetExtent() # extent (-4.0699641, -3.2300143, 40.170042, 40.6499669)
+    layer = data_source.GetLayer()
+    extent = layer.GetExtent()
+    # extent = (-4.0699641, -3.2300143, 40.170042, 40.6499669)
     exmidx = extent[0] - ((extent[0] - extent[1]) / 2)
-    exmidy=extent[2]-((extent[2]-extent[3])/2)
+    exmidy = extent[2] - ((extent[2] - extent[3]) / 2)
+    # extent_center = Point(x, y)
     # print('extent', extent, exmidx)
-    p=Point(exmidx+0.05,exmidy+0.04)
-    point_buff=p.buffer(0.2)
-    point_bounds=point_buff.bounds
-    sbox=box(point_bounds[0],point_bounds[1],point_bounds[2],point_bounds[3])
+    # point to geometry for SQL statement
+    p = Point(exmidx + 0.05, exmidy + 0.04)
+    point_buff = p.buffer(0.2)
+    point_bounds = point_buff.bounds
+    sbox = box(point_bounds[0], point_bounds[1], point_bounds[2], point_bounds[3])
     park_layer = data_source.ExecuteSQL(
-        sql_park,    
-         ogr.CreateGeometryFromWkt(to_wkt(sbox))
+        sql_park,
+        ogr.CreateGeometryFromWkt(to_wkt(sbox))
     )
-    scale=1000
-    polys=[]
+    scale = 1000
+    polys = []
     for f in park_layer:
         geo_json = f.ExportToJson()
-        fgj=from_geojson(geo_json)
-        if(is_valid(fgj)==True):
+        fgj = from_geojson(geo_json)
+        if is_valid(fgj):
             polys.append(fgj)
-    # polys.append(point_buff)
+    # set up the style here
     polys.append(sbox)
-    mp=MultiPolygon(polys)    
+    mp = MultiPolygon(polys)
     scaled_poly = affinity.scale(
         mp,
-        scale,-scale
-        )
-    features_park = Features(park_layer, item_scale, group_scale)
+        scale, -scale
+    )
     svg_tag = SVGTag(styles)
-    # svg_tag.prepend(spat_box2_scaled.svg(),'extent-level-1')
-    svg_tag.append(sbox.svg(),'target_area')
-    svg_tag.append(scaled_poly.svg(), '')
+    # svg_tag.append(sbox.svg(), 'target_area')
+    svg_tag.append(scaled_poly.svg(), 'a')
     return svg_tag.render()
--- run_script.py
+++ run_script.py
@@ -5,8 +5,8 @@ item_scale = 2
 group_scale = 600
 data_path = 'data/Madrid-shp/shape/natural.shp'
 file = open('madrid/style.css', 'r', encoding='utf-8') 
-t=time.strftime('%X', time.localtime())
-destination_path = '__generated/madrid_2023_11_27{}.svg'.format('')
+t = time.strftime('%X', time.localtime())
+destination_path = '__generated/madrid_2023_11_28{}.svg'.format('')
 
 svg_tag=natural_2(item_scale,group_scale,data_path,file.read())
 
