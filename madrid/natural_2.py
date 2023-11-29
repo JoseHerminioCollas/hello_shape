af5c2af4d7db2c0c162d304ca378d75b90c50347
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-11-29 15:22:53 -0500

    view spat with it on and with it off

--- madrid/natural_2.py
+++ madrid/natural_2.py
@@ -4,54 +4,41 @@ from shapely import MultiPolygon, affinity, box, Point, from_geojson, is_valid, 
 from osgeo import ogr
 
 
-def extent_box(extent):
-    exmidx = extent[0] - ((extent[0] - extent[1]) / 2)
-    exmidy = extent[2] - ((extent[2] - extent[3]) / 2)
-    p = Point(exmidx + 0.0, exmidy + 0.04)
-    point_buff = p.buffer(0.2)
-    point_bounds = point_buff.bounds
-    sbox = box(point_bounds[0], point_bounds[1], point_bounds[2], point_bounds[3])
-    return sbox
-
-
 def natural_2(
         item_scale,
         group_scale,
         data_path,
         styles,
-        spatial_filter=None,
         cx=None,
         cy=None,
         buffer=0.2,
-        view_spat_area=True,
         use_spat=True,
+        view_spat_area=True,
 ):
-    sql_park = ("SELECT * FROM natural where type='park' and name is not null limit {}"
-                .format(50000))
+    polys = []
     data_source = ogr.Open(data_path)
     layer = data_source.GetLayer()
     extent = layer.GetExtent()
-    # extent = (-4.0699641, -3.2300143, 40.170042, 40.6499669)
-    box_a = extent_box(extent)
-    # make the ogr_geo from the extent
-    ogr_geo = ogr.CreateGeometryFromWkt(to_wkt(box_a))
-    # make the ogr_geo from a point
+    # TODO if center not in extent throw error
+    # make a Shapely box of the SPAT filter inputs: cx, cy, buffer
     p = Point(cx, cy)
     point_buff = p.buffer(buffer)
     point_bounds = point_buff.bounds
-    box_b = box(point_bounds[0], point_bounds[1], point_bounds[2], point_bounds[3])
-    ogr_geo_b = None
+    geo_spat_box = box(point_bounds[0], point_bounds[1], point_bounds[2], point_bounds[3])
+    if view_spat_area:
+        polys.append(geo_spat_box)
+    # make the geospatial filter if requested
+    geospatial_filter = None
     if use_spat:
-        ogr_geo_b = ogr.CreateGeometryFromWkt(to_wkt(box_b))
-
+        geospatial_filter = ogr.CreateGeometryFromWkt(to_wkt(geo_spat_box))
+    # get the layer
+    sql_park = ("SELECT * FROM natural where type='park' and name is not null limit {}"
+                .format(50000))
     park_layer = data_source.ExecuteSQL(
         sql_park,
-        ogr_geo_b
+        geospatial_filter
     )
-    scale = group_scale
-    polys = []
-    if view_spat_area:
-        polys.append(box_b)
+    # populate polygons
     for f in park_layer:
         geo_json = f.ExportToJson()
         fgj = from_geojson(geo_json)
@@ -60,8 +47,9 @@ def natural_2(
     mp = MultiPolygon(polys)
     scaled_poly = affinity.scale(
         mp,
-        scale, -scale
+        group_scale, -group_scale
     )
+    # set the SVGTag object
     svg_tag = SVGTag(styles)
     svg_tag.append(scaled_poly.svg(), 'a')
     return svg_tag.render()
--- run_script.py
+++ run_script.py
@@ -2,20 +2,20 @@ import time
 from madrid.natural_2 import natural_2
 
 item_scale = 2
-group_scale = 600
+group_scale = 300
 data_path = 'data/Madrid-shp/shape/natural.shp'
 file = open('madrid/style.css', 'r', encoding='utf-8')
+# extent = (-4.0699641, -3.2300143, 40.170042, 40.6499669)
+# generate the series here
 t = time.strftime('%X', time.localtime())
-destination_path = '__generated/madrid_2023_11_28{}.svg'.format('')
-
+destination_path = '__generated/madrid_2023_11_29{}.svg'.format('-b')
 svg_tag = natural_2(
     item_scale, group_scale,
     data_path, file.read(),
-    None,
     -4.0, 40.3,
-    0.2, True, False
+    0.2,
+    True, True
 )
-
 file = open(destination_path, 'w')
 file.write(svg_tag)
 print('File created: ', file)
