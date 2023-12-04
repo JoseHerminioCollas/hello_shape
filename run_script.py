3f488d25b593e996efb5fab18f647887610fd88c
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-12-04 16:55:38 -0500

    refactor name of function

--- run_script.py
+++ run_script.py
@@ -1,5 +1,5 @@
 import time
-from madrid.natural_2 import natural_2
+from madrid.natural import natural
 
 item_scale = 1
 group_scale = 500
@@ -8,8 +8,8 @@ file = open('madrid/style.css', 'r', encoding='utf-8')
 # extent = (-4.0699641, -3.2300143, 40.170042, 40.6499669)
 # generate the series here
 t = time.strftime('%X', time.localtime())
-destination_path = '__generated/madrid_zoom_2023_12_1{}.svg'.format('-b')
-svg_tag = natural_2(
+destination_path = '__generated/madrid_zoom_2023_12_4{}.svg'.format('')
+svg_tag = natural(
     item_scale, group_scale,
     data_path, file.read(),
     -4.0, 40.3,
